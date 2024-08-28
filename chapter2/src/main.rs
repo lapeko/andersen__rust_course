e3ec0cd269d7468328b0be1f34a089640c2f3234
lapeko <lapeko88@gmail.com>
2024-08-28 20:36:56 +0200

    handle terminal input

--- chapter2/src/main.rs
+++ chapter2/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;@
+
 fn main() {
-    println!("Hello, world!");
+    let mut user_input = String::new();
+
+    println!("Write down something");
+
+    match io::stdin().read_line(&mut user_input) {
+        Ok(_) => {
+            println!("You wrote \"{}\"", user_input.trim())
+        },
+        Err(err) => {
+            println!("Something went wrong {}", err)
+        }
+    }
 }
